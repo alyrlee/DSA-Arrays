7243a2d483f4fc39e37ab8e1a778c1d9f7ddd8d5
Ashley Lee <53842786+alyrlee@users.noreply.github.com>
2020-09-19 17:16:52 -0400

    Update dsa-arrays-drills.js

--- dsa-arrays-drills.js
+++ dsa-arrays-drills.js
@@ -2,16 +2,39 @@
 5. URLify a string
 A common mistake users make when they type in an URL is to put spaces between words or letters. A solution that developers can use to solve this problem is to replace any spaces with a %20. Write a method that takes in a string and replaces all its empty spaces with a %20. Your algorithm can only make 1 pass through the string. Examples of input and output for this problem can be
 
-Input: tauhida parveen
+Input: Ashley Lee
 
-Output: tauhida%20parveen
+Output: Ashley%20Lee
 
-Input: www.thinkful.com /tauh ida parv een
+Input: www.thinkful.com /ash ley lee
 
-Output: www.thinkful.com%20/tauh%20ida%20parv%20een
+Output: www.thinkful.com%20/ash%20ley%20lee
+============*/
+
+'use strict';
+
+function urlifyString(string) {
+  string = string.trim();
+  let output = '';
+  for (let i = 0; i < string.length; i++) {
+    if (string[i] === ' ') {
+      output += '%20';
+    } else {
+      output += string[i];
+    }
+  }
+  return output;
+}
+
+console.log(urlifyString('Ashley Lee'));
+
+// complexity: O (n)
+
+/*============
 
 6. Filtering an array
-Imagine you have an array of numbers. Write an algorithm to remove all numbers less than 5 from the array. DO NOT use Array's built-in .filter() method here; write the algorithm from scratch.
+Imagine you have an array of numbers. Write an algorithm to remove all numbers less than 5 from the array. 
+DO NOT use Array's built-in .filter() method here; write the algorithm from scratch.
 
 7. Max sum in the array
 You are given an array containing positive and negative integers. Write an algorithm which will find the largest sum in a continuous sequence.
